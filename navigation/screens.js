f77ccdaafe20c089dad4eeeae85ee30f287ee00b
Joan Artes <joan@joanartes.com>
2019-09-23 16:28:17 +0200

    SimpleScreenComponent deconstruction

--- navigation/screens.js
+++ navigation/screens.js
@@ -1,7 +1,7 @@
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import HomeScreen from '../screens/home-screen/home-screen.component';
-import SimpleScreen from '../screens/simple-screen/simple-screen.component';
+import SimpleScreen from '../screens/simple-screen/simple-screen.container';
 import ReduxScreen from '../screens/redux-screen/redux-screen.container';
 import SagasScreen from '../screens/sagas-screen/sagas-screen.container';
 
