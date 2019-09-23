bee1d61a1595cdcb28c2da97362f534516bf7a2d
Joan Artes <joan@joanartes.com>
2019-09-23 15:16:52 +0200

    Random Colors Generator

---
+++ redux/sagas/color-sagas.js
@@ -0,0 +1,8 @@
+function* randomColors() {	
+	yield '#008000'
+	yield '#00CED1'
+	yield '#BA55D3'
+	yield '#D2691E'
+}
+
+export default randomColors
--- screens/simple-screen/simple-screen.component.js
+++ screens/simple-screen/simple-screen.component.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
-import {  View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+import randomColors from './simple-screen.handlers'
+
+const randomColor = randomColors();
 
 export default class SimpleScreen extends Component {
+
+  constructor(props) {
+    super(props);
+      this.state = {
+        color: randomColor.next().value,
+    };
+  }
+  
+    _updateColor() {
+      let nextColor = randomColor.next()
+      console.log( nextColor );
+      this.setState({ color: nextColor.value }) 
+    }
+
   render() {
+
     return (
       <View>
-        <Text> Simple Screen </Text>
+        <TouchableOpacity onPress={ () => this._updateColor() } style={{ borderColor: 'red', borderWidth: 1 }} >
+          <Text style={{color: this.state.color}} >{this.state.color}</Text>
+        </TouchableOpacity>
       </View>
     );
   }
