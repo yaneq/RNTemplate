c64cdde40807c07717a00af30050f1fa21cf6e33
Jan Koeppen <jan@yaneq.com>
2019-09-23 11:21:17 +0200

    add dev tools support

--- App.js
+++ App.js
@@ -4,10 +4,16 @@ import PropTypes from 'prop-types'
 import NavigationContainer from './navigation/screens'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './redux/reducers/root-reducer'
 
 function configureStore() {
-  const store = createStore(rootReducer, applyMiddleware())
+  const store = createStore(
+    rootReducer,
+    composeWithDevTools(
+      applyMiddleware()
+    )
+  )
   return store
 }
 
