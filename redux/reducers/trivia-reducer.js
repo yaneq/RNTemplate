557dd5fda4b24d7686021147e2ec3e12de7a508b
Jan Koeppen <jan@yaneq.com>
2019-10-03 18:33:20 -0400

    add saga dummy

--- redux/reducers/trivia-reducer.js
+++ redux/reducers/trivia-reducer.js
@@ -1,20 +1,19 @@
-import { TRIVIA_ACTIONS } from '../actions/trivia-actions'
+import {TRIVIA_ACTIONS} from '../actions/trivia-actions';
 
 const initialState = {
   question: 'Home many countries are in the EU?',
-  loading: false
-}
+  loading: false,
+};
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case TRIVIA_ACTIONS.LOAD_QUESTION:
+      return {
+        ...state,
+        loading: true,
+      };
 
-  case TRIVIA_ACTIONS.LOAD:
-    return {
-      ...state,
-      loading: true
-    };
-
-  default:
-    return state
+    default:
+      return state;
   }
 };
--- screens/sagas-screen/sagas-screen.component.js
+++ screens/sagas-screen/sagas-screen.component.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react'
-import { Text, View, TouchableOpacity } from 'react-native'
+import React, {Component} from 'react';
+import {Text, View, TouchableOpacity} from 'react-native';
 
 export default class SagasScreenComponent extends Component {
   render() {
-    console.log('props', this.props)
-    let { loadQuestion, trivia } = this.props;
+    let {loadQuestion, trivia} = this.props;
     return (
       <View>
         <Text> Sagas screen </Text>
-        <Text> Question {trivia.loading}</Text>
+        <Text> Question {trivia.loading ? 'loading' : 'done'}</Text>
         <TouchableOpacity onPress={loadQuestion}>
           <Text>Get new question</Text>
         </TouchableOpacity>
       </View>
-    )
+    );
   }
 }
